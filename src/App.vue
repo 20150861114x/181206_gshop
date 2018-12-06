ba8543551fa5964f03569cd5001d7db4fc14e76e
e <757931751@qq.com>
2018-12-06 18:09:24 +0800

    'gshop'

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view></router-view>
+    <FooterGuide v-show="$route.meta.showFooter"></FooterGuide>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide'
+
+export default {
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style>
+
+</style>
